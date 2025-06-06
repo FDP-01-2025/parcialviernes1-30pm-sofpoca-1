e75a28d972b1e72ce9d5c6e393f175a2ad9d4066
sofpoca <00005225@uca.edu.sv>
2025-06-06 15:41:27 -0600

    :art:

--- puntosExtra2.cpp
+++ puntosExtra2.cpp
@@ -6,27 +6,33 @@ using namespace std;
 int main(){
     int total=0;
     cout<<"Bienvenido a la tiendita UCA :b\n";
+    //guardamos los productos
     string productos[5]={"Tomates", "Banana", "Lechuga", "Frijoles", "Arroz"};
     vector<int> carrito(5, 0);
     vector<int> disponibilidad(5, 10);
+    //llevamos dos vectores uno por la cantidad inicial de productos y el otro por cuanto vayamos comprando
     char opcion;
     int x;
     do{
         cout<<"Ingresa la opcion que deseas realizar: "<<endl;
         cout<<" \ta. Ver productos\n \tb.Agregar producto al carrito\n \tc. Ver carrito de compras\n \td.Finalizar compra y salir\n";
         cin>>opcion;
+        //se muestra el menu y ingresa la opcion
         switch(opcion){
             case 'a':
-            cout<<"Productos disponibles: \n";
+                cout<<"Productos disponibles: \n";
                 cout<<"Precio\t\t Producto\t\t Cantidad diponible\n";
+                //mostramops todos los productos disponibles, si no es disponible lo saltamos
                 for(int i=0;i<5;i++){
                     if(disponibilidad[i]>0){
                         cout<<"$1.0 \t\t"<<productos[i]<<": \t\t "<<disponibilidad[i]<<endl;
+                        //mostamos precio, prducto y cantidad disponible
                     }
                 }
                 break;
             case 'b':
                 cout<<"Ingrese el producto desea agregar al carrito: \n";
+                //mostramos que productos puede agregar al carrito
                 for(int i=0;i<5;i++){
                     cout<<i+1<<". "<<productos[i]<<endl;
                 }
@@ -42,6 +48,7 @@ int main(){
             case 'c':
                 cout<<"Tu carrito de compras es:\n";
                 cout<<"Producto \t\tCantidad\n";
+                //mostraos los produtos y cantida en el carritp
                 for(int i=0;i<5;i++){
                     cout<<productos[i]<<": \t\t"<<carrito[i]<<endl;
                 }
@@ -51,14 +58,18 @@ int main(){
                 for(int i=0;i<5;i++){
                     x+=carrito[i];
                 }
+                //se suman los precios
                 cout<<"El total de tu compra es: $"<<x<<endl;
                 return 0;
+                //fin
             default:
+            //la opcion no era valida
             cout<<"Opcion invalida\n";
             break;
                 
         }
     } while(true);
     cout<<"Fin de el programa :)\n";
+   // fin nuevamebte 
     return 0;
 }
